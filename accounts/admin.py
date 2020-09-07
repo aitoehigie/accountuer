25797868a08ee1bb54850e73d4c24cbfa4c34ace
ehigie aito <aitoehigie@gmail.com>
2020-09-07 16:59:38 +0100

    create the home app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+from django.urls import reverse, reverse_lazy
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display =['username', 'email', 'age']
+    list_filter = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.urls import reverse, reverse_lazy
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(null=True, blank=True)
 
     def get_absolute_url(self):
         return revere_lazy('home')
